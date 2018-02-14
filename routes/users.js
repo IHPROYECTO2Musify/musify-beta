78e00c57941449870d959f324fdef85f55ddf53a
JurgenBlitz <jorge.gopae@gmail.com>
2018-02-14 16:47:35 +0100

    user to ad link works

--- routes/users.js
+++ routes/users.js
@@ -33,14 +33,7 @@ userRoutes.post(
   "/new-user",
   ensureLoggedIn("/auth/login"),
   (req, res, next) => {
-    const updates = ({
-      username,
-      city,
-      description,
-      mainInstrument,
-      otherInstrument,
-      experience
-    } = req.body);
+    const updates = ({username, city, description, mainInstrument, otherInstrument, experience} = req.body);
     console.log(updates);
     if (username === "" || mainInstrument === "" || city === "") {
       res.render("users/new-user", {
@@ -57,13 +50,56 @@ userRoutes.post(
     });
   }
 );
-
-userRoutes.get("edit/:id", (req, res, next) => {
-  res.render("users/edit");
-});
-
+//Send into private page
 userRoutes.get("/activity", (req, res, next) => {
   res.render("users/activity");
 });
 
+//start the user edit process
+userRoutes.get("/:id/edit", ensureLoggedIn('/login'), (req, res, next) => {
+  User.findById(req.user._id, (err, us) => {
+    if (err) { return next(err) }
+    if (!ad) { return next(new Error("404")) }
+  return res.render("users/edit", {city: City, mainInstrument: Instrument, otherInstrument: Instrument, experience: Experience});
+  });
+}); 
+
+userRoutes.post('/:id/edit', ensureLoggedIn('/login'), (req, res, next) => {
+const updates = { imgUrl, username, city, description, styles, mainInstrument, otherInstrument, experience} = req.body;
+
+
+User.findByIdAndUpdate(req.user._id, updates, { new: true }, (err, user) => {
+  if (err) {
+      //req.flash('info','Errores al editar');
+      return res.render('users/edit', {
+          us,
+          errors: us.errors
+      });
+  }
+  if (!us) {
+      return next(new Error('Error al editar, el usuario no existe'));
+  }
+  //req.flash('info','Datos editados');
+  req.user = user;
+  return res.redirect(`/users/activity/${user._id}`);
+});
+});
+
 module.exports = userRoutes;
+
+
+
+
+
+      
+
+
+// router.get("/:id/delete", (req, res) =>{
+//   const id = req.params.id;
+//   Ad.findById(id).exec((err, ad) => {
+//     ad.remove({},(err) =>{
+//       res.redirect("/ad/list");
+//     })
+    
+//   });
+// })
